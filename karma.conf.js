3406e62ca5d743446517c8b1a365e56b410bee23
Raphael Akpan <raphael.akpan@andela.com>
2016-11-16 13:40:58 +0100

    Refactor to follow standards

--- karma.conf.js
+++ karma.conf.js
@@ -9,7 +9,6 @@ module.exports = function(config) {
 
 
     // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jasmine'],
 
 
@@ -25,14 +24,11 @@ module.exports = function(config) {
 
 
     // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
     },
 
 
     // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
     reporters: ['spec'],
 
 
@@ -45,16 +41,13 @@ module.exports = function(config) {
 
 
     // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
 
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: true,
+   autoWatch: true,
 
 
     // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome', 'Firefox', 'PhantomJS'],
 
 
@@ -65,5 +58,5 @@ module.exports = function(config) {
     // Concurrency level
     // how many browser should be started simultaneous
     concurrency: Infinity
-  })
-}
+  });
+};
--- test/user/example.spec.js
+++ test/user/example.spec.js
@@ -1,7 +1,6 @@
 describe('User Example', function() {
-    var user = {}
 
-    it('Dummy test example', function() {
-         expect(2 + 2).toEqual(4);
+    it('Dummy User test example', function() {
+         expect(2).toEqual(4);
     });
 });
