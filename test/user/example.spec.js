b0875aff8bef3c4389ba92db8b6ce1f66a5b4079
andela-oakinseye <olawale.akinseye@andela.com>
2016-11-21 16:46:29 +0100

    update test spec example

--- test/user/example.spec.js
+++ test/user/example.spec.js
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 describe('User Example', function () {
-  it('Dummy User test example', function () {
-    expect(4).toEqual(4);
-  });
-=======
-describe('User Example', function() {
-
-    it('Dummy User test example', function() {
-         expect(4).toEqual(4);
+    it('Dummy User test example', function () {
+      expect(4).toEqual(4);
     });
->>>>>>> 305e37004212b42b5db918608d745b5961350831
-});
+  });
