4b6aca51c8f92bc1a6eb6b6c0bdb0ac85d5fe1a1
Olaniran Azeez <azeez.olaniran@andela.com>
2016-11-22 10:59:46 +0100

    chore(code coverage): rewrite example.js using es5 syntax

---
+++ app/example.js
@@ -0,0 +1,21 @@
+/*class Example {
+	addNumbers(a, b){
+		return (a + b);
+	}
+
+	multiplyNumbers(a, b){
+		return (a * b);
+	}
+}
+var example = {
+	addNumbers = function(a, b){
+		return (a * b);
+	}
+}*/
+
+function Example(){
+	this.addNumbers = function(a, b){
+		return a * b;
+	}
+}
+
--- test/user/example.spec.js
+++ test/user/example.spec.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
-//const example = new Example();
+const example = new Example();
 describe('User Example', function () {
   it('should return summation of 2 numbers', function () {
-    expect(4).toEqual(4);
+    expect(example.addNumbers(2, 2)).toEqual(4);
   });
 });
