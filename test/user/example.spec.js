47e4a89ee14e92d5c8c447a5da0aa13df3400214
Olaniran Azeez <azeez.olaniran@andela.com>
2016-11-22 14:22:25 +0100

    chore(code coverage): delete example test file and fix missing / in karma config file

--- test/user/example.spec.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable */
-const example = new Example();
-describe('User Example', function () {
-  it('should return summation of 2 numbers', function () {
-    expect(example.addNumbers(2, 2)).toEqual(4);
-  });
-});
