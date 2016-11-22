88e85cdcd26e212dc8940a3c01eed9ca70582e3f
Olaniran Azeez <azeez.olaniran@andela.com>
2016-11-22 11:36:36 +0100

    chore(code coverage): modify code to adhere to style guide and ignore coverage folder

--- app/example.js
+++ app/example.js
@@ -1,21 +1,6 @@
-/*class Example {
-	addNumbers(a, b){
-		return (a + b);
-	}
-
-	multiplyNumbers(a, b){
-		return (a * b);
-	}
-}
-var example = {
-	addNumbers = function(a, b){
-		return (a * b);
-	}
-}*/
-
 function Example(){
-	this.addNumbers = function(a, b){
-		return a * b;
-	}
+  this.addNumbers = function(a, b){
+    return a * b;
+  };
 }
 
