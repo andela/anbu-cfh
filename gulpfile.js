6677bca4a46db657f761116745c1884f9b090512
Oreoluwa Banwo <oreoluwa.banwo@andela.com>
2016-11-23 13:33:34 +0100

    updated gulfile indenting issue

--- gulpfile.js
+++ gulpfile.js
@@ -11,29 +11,29 @@ require('dotenv').config();
   var port = process.env.PORT;
 
 gulp.task('watch', () => {
-	gulp.watch('public/css/common.scss', ['sass']);
- 	gulp.watch('public/css/**', browserSync.reload);
- 	gulp.watch('public/views/**', browserSync.reload);
- 	gulp.watch(['public/js/**', 'app/**/*.js'], browserSync.reload);
-	gulp.watch('app/views/**', browserSync.reload);
+  gulp.watch('public/css/common.scss', ['sass']);
+  gulp.watch('public/css/**', browserSync.reload);
+  gulp.watch('public/views/**', browserSync.reload);
+  gulp.watch(['public/js/**', 'app/**/*.js'], browserSync.reload);
+  gulp.watch('app/views/**', browserSync.reload);
 
  });
 
 
 gulp.task('sass', () => {
-	return gulp.src('public/css/common.scss')
-	.pipe(sass())
-	.pipe(gulp.dest('public/css/'));
+  return gulp.src('public/css/common.scss')
+  .pipe(sass())
+  .pipe(gulp.dest('public/css/'));
 });
 
 gulp.task('eslint', () => {
-	return gulp.src(['gulpfile.js', 'public/js/**/*.js', 'test/**/*.js', 'app/**/*.js'])
-	.pipe(eslint());
+  return gulp.src(['gulpfile.js', 'public/js/**/*.js', 'test/**/*.js', 'app/**/*.js'])
+  .pipe(eslint());
 });
 
 gulp.task('bower', () => {
-	return bower()
-	.pipe(gulp.dest('./public/lib'));
+  return bower()
+  .pipe(gulp.dest('./public/lib'));
 });
 
 gulp.task('mochaTest', () => {
