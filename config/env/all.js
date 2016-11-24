dcb86c35c8fa174bd73fcb35fb5fdbd5983a51a6
andela-oakinseye <olawale.akinseye@andela.com>
2016-11-24 17:15:50 +0100

    refactor to meet sever specification

--- config/env/all.js
+++ config/env/all.js
@@ -1,10 +1,10 @@
 /* eslint-disable */
 var path = require('path'),
-  rootPath = path.normalize(__dirname + '/../..');
-let keys = `${rootPath}/keys.txt`;
+rootPath = path.normalize(__dirname + '/../..');
+var keys = rootPath + '/keys.txt';
 
 module.exports = {
-  root: rootPath,
-  port: process.env.PORT,
-  db: process.env.MONGODB_URL
+	root: rootPath,
+	port: process.env.PORT,
+    db: process.env.MONGODB_URL
 };
