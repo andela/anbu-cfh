c6f00d95e858c7bf0ef52edda91399e7a9745e7f
andela-oakinseye <olawale.akinseye@andela.com>
2016-11-24 14:40:12 +0100

    fix indentation issue

--- config/env/all.js
+++ config/env/all.js
@@ -1,9 +1,9 @@
 var path = require('path'),
-rootPath = path.normalize(__dirname + '/../..');
+  rootPath = path.normalize(__dirname + '/../..');
 var keys = rootPath + '/keys.txt';
 
 module.exports = {
-	root: rootPath,
-	port: process.env.PORT,
-    db: process.env.MONGODB_URL
+  root: rootPath,
+  port: process.env.PORT,
+  db: process.env.MONGODB_URL
 };
