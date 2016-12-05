b377cbfc151041209e7624e4bbeea8309b6a2c07
andela-oakinseye <olawale.akinseye@andela.com>
2016-12-05 23:35:49 +0100

    add script to fix mdl injected DOM item

---
+++ public/js/mutateDOM.js
@@ -0,0 +1,22 @@
+/** Updates the DOM when nodes are added */
+/** State teansitions cause node to be added  */
+const observer = new MutationObserver((mutations) => {
+  let upgrade = false;
+
+  for (let i = 0; i < mutations.length;) {
+    if (mutations[i].addedNodes.length > 0) {
+      upgrade = true;
+      break;
+    }
+    i += 1;
+  }
+
+  if (upgrade) {
+    window.componentHandler.upgradeDom();
+  }
+});
+
+observer.observe(document, {
+  childList: true,
+  subtree: true
+});
