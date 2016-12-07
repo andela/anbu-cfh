806c43320b3e397e30500bf6d7563e6fcb9a7f17
andela-oakinseye <olawale.akinseye@andela.com>
2016-12-07 12:54:43 +0100

    update script reference

--- public/js/mutateDOM.js
+++ public/js/mutateDOM.js
@@ -1,6 +1,8 @@
 /** Updates the DOM when nodes are added */
 /** State transitions cause node to be added  */
-/** reference https://getmdl.io/started/index.html */
+/** References: https://getmdl.io/started/index.html , 
+ * https://developers.google.com/web/updates/2012/02/Detect-DOM-changes-with-Mutation-Observers
+ **/
 const observer = new MutationObserver((mutations) => {
   let upgrade = false;
 
