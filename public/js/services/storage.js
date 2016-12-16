690686c491148f508770a3cbf6d5a0a3b5de10fb
Raphael Akpan <raphael.akpan@andela.com>
2016-12-16 20:53:47 +0100

    Add Storage Service for Local Storage

---
+++ public/js/services/storage.js
@@ -0,0 +1,17 @@
+
+angular.module('mean.system')
+  .factory('LocalStorage', ['localStorageService', function(localStorageService) {
+    return {
+      storeToken: function(key, value) {
+        return localStorageService.set(key, value);
+      },
+
+      getToken: function(key) {
+        return localStorageService.get(key);
+      },
+
+      clearToken: function(key) {
+        return localStorageService.remove(key);
+      },
+    }
+}]);
