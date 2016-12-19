fee52f87f685217eda08ac01621bcb373e00c652
Raphael Akpan <raphael.akpan@andela.com>
2016-12-19 11:41:19 +0100

    Remove console.log

--- public/js/services/interceptor.js
+++ public/js/services/interceptor.js
@@ -1,13 +1,12 @@
 angular.module('mean.system')
   .factory('AuthInterceptor', ['LocalStorage', function(LocalStorage) {
     var AuthInterceptor = {
-      request: function(config) {
-          if (LocalStorage.getToken('token')) {
-              config.headers['x-access-token'] = LocalStorage.getToken('token');
-              console.log(config);
-          }
-          return config;
+    request: function(config) {
+      if (LocalStorage.getToken('token')) {
+        config.headers['x-access-token'] = LocalStorage.getToken('token');
       }
+      return config;
+    }
   };
   return AuthInterceptor;
 }]);
